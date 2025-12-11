6c9658367c78ab081abff86fc618720fb7e2de68
cblack267 <cblack267@icloud.com>
2025-12-11 00:40:19 -0500

    Added skeletons, search and pagination.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,12 +32,17 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log("Fetching invoice data...");
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log("Data fetch completed after 3 seconds.");
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
@@ -55,6 +60,10 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+    console.log("Fetching invoice data...");
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
@@ -67,6 +76,8 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+
+    console.log("Data fetch completed after 3 seconds.");
 
     const numberOfInvoices = Number(data[0][0].count ?? "0");
     const numberOfCustomers = Number(data[1][0].count ?? "0");
